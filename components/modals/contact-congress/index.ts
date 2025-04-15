8f520301abdf1995d8236628b8ec976adfac00eb
solovyovsdom <joelborofsky@mac.com>
2025-04-15 01:44:19 -0500

    Remove unused contact congress modals to streamline component structure

--- components/modals/contact-congress/index.ts
+++
@@ -1,4 +0,0 @@
-export { CallRepModal } from "./call-rep-modal/CallRepModal";
-export { CallSenatorsModal } from "./call-senators-modal/CallSenatorsModal";
-export { EmailRepModal } from "./email-rep-modal/EmailRepModal";
-export { EmailSenatorsModal } from "./email-senators-modal/EmailSenatorsModal";
